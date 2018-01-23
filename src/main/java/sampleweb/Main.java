8ee87adae48cfa5557021ea5a6242f8556935524
Satheeshkumar Ravichandran <satheeshkumar.r@payoda.com>
2018-01-23 22:16:02 +0530

    Update Main.java

--- src/main/java/sampleweb/Main.java
+++ src/main/java/sampleweb/Main.java
@@ -1,7 +1,7 @@
 package sampleweb;
 
 public class Main {
-	private String imageURL="https://unifaceinfo.com/wp-content/uploads/2017/08/DevOps-Gear.png2";
+	private String imageURL="https://unifaceinfo.com/wp-content/uploads/2017/08/DevOps-Gear.png";
 	public String getImageURL() {		
 		return imageURL;
 	}
